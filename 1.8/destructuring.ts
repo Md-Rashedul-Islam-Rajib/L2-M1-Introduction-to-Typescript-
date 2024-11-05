0514202a6926e0f3732c5c450f1281719b48669c
Md Rashedul Islam <rajib5570@gmail.com>
2024-11-06 00:26:06 +0600

    destructuring typescript added

---
+++ 1.8/destructuring.ts
@@ -0,0 +1,29 @@
+{
+
+    // destructuring in TypeScript
+
+    // object destructuring
+
+    const person = {
+        name: {
+            firstName: "Mr",
+            secondName: "Hasan",
+            lastName: "Islam"
+        },
+        age: 29
+    }
+
+
+    const { age, name: { secondName } } = person;
+
+    //array destructuring
+
+    const people:string[] = ['abul', 'samad', 'kamal', 'jamal', 'rahim', 'karim'];
+
+    const [ ,employee, teacher, neighbour,...otherPeople] = people;
+
+
+
+
+
+}
