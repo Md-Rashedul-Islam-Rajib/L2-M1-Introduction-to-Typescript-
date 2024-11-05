d1144133a3400fc3797b845fd42741ad5a1e7156
Md Rashedul Islam <rajib5570@gmail.com>
2024-11-05 21:24:47 +0600

    basic data type 1.4 added

---
+++ 1.4/basicDataType.ts
@@ -0,0 +1,33 @@
+{
+  //Basic data type of typescript
+
+  //primitive data type
+
+  // string
+  const myName: string = "Mr X";
+
+  //number
+  const age: number = 99;
+
+  //boolean
+  const isEmployed: boolean = false;
+
+  //undefined
+  const x: undefined = undefined;
+
+  // null
+    const y: null = null;
+    
+    //non-primitive data type
+
+    //array
+    let friends: string[] = ['a', 'b', 'c'];
+    let friendsAge: number[] = [11, 12, 13];
+
+    //tuple / ordered-array / controled-index-array
+    let coordinates: [number, number] = [63, 25];
+    let person: [string, number,boolean] = ["Mr x", 69, true];
+
+
+
+}
---
+++ dist/1.4/basicDataType.js
@@ -0,0 +1,22 @@
+"use strict";
+{
+    //Basic data type of typescript
+    //primitive data type
+    // string
+    const myName = "Mr X";
+    //number
+    const age = 99;
+    //boolean
+    const isEmployed = false;
+    //undefined
+    const x = undefined;
+    // null
+    const y = null;
+    //non-primitive data type
+    //array
+    let friends = ['a', 'b', 'c'];
+    let friendsAge = [11, 12, 13];
+    //tuple / ordered-array / controlled-index-array
+    let coordinates = [63, 25];
+    let person = ["Mr x", 69, true];
+}
